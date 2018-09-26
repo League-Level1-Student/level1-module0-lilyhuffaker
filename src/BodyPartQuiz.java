d97de444432d2fbd47f9646646ba8b005257dcc3
Huffaker.Lily <Huffaker.Lily@SEMS-259-32.SEMS259.LAB>
2018-09-26 16:23:36 -0700

    et barn yeet

--- src/BodyPartQuiz.java
+++ src/BodyPartQuiz.java
@@ -31,9 +31,9 @@ public class BodyPartQuiz {
 	private void startQuiz() {
 
 		// 1. Make an int variable to hold the score.
-
+		int score = 0;
 		// 2. Set the size of the window in the initializeGui() method below
-
+		
 		for (int i = 0; i < 4; i++) {
 			
 			// 4. Ask the user who this person is and store their answer
@@ -41,17 +41,31 @@ public class BodyPartQuiz {
 			
 			// 5. Check their answer. If they guessed correctly:
 			// -- Tell them they are right and increase the score by 1
-
+			if(guess.equals("Arnold")) {
+				JOptionPane.showMessageDialog(null, "Wow, you did the gucci gang.");
+				score++;
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "No you Poopy butt, I am the dissapoint.");
+			}
 			// 6. Otherwise:
 			// -- Tell them they are wrong and who the person is
 
 			// 7. Use the showNextImage() method below to get the next image
 			showNextImage();
 		    // 8. Show them their current score
-			
+			JOptionPane.showMessageDialog(null, "your score is:"+score);
 			// 9. .... repeat for all your images.....
-
-
+			String guessdos= JOptionPane.showInputDialog("Who is this boyo?");
+			if (guessdos.equals("Leoardo")) {
+				JOptionPane.showMessageDialog(null, "yEETUS THAT FEETUS, GOOD JOB MY DUDE");
+				score++;
+		}
+			else {
+				JOptionPane.showMessageDialog(null, "Oof thats wrong.");
+			}
+			JOptionPane.showMessageDialog(null, "uR SCORE IS: "+score);
+				//ur working on this boyo /\/\/\//\/\///\/\
 		}
 
 	}
@@ -75,7 +89,7 @@ public class BodyPartQuiz {
 		window.add(panel);
 		
 		// 3. Change the size of the window so that you can only see part of the image.		
-		window.setSize(500,500);
+		window.setSize(200,200);
 		
 		showNextImage();
 		
--- src/DrumKit.java
+++ src/DrumKit.java
@@ -52,21 +52,23 @@ public class DrumKit implements MouseListener {
    	 // 11. Set the layout of the panel to "new GridLayout()"
     panel.setLayout(new GridLayout());
       	 // 12. call the pack() method on the frame.  Run your program. Do you see your drum image?
-   	  drum.pack();
+   	 drum.pack();
    	 // 13. add this mouse listener to drumLabelWithImage
-//Your working on this! /\ /\ /\
+   	 drumLabelWithImage.addMouseListener(this);
      	 // 18. Add more images to make a drumkit. Remember to add this mouse listener to each one.
 
     }
 
     public void mouseClicked(MouseEvent e) {
    	 // 14. Print "mouse clicked" to the console. Run your program and watch the console to see when this is printed.
-
+    	System.out.println("mOUSE cLICKED");
    	 JLabel drumClicked = (JLabel) e.getSource();  // This line gets the label that the mouse clicked on
    	 
    	 // 15. Download a drum sound and drop it into your "default package". You can find it on freesound.org. To download it, log in as leagueofamazing/code4life.
    	 // 16. If they clicked on the drumImage...
-
+  
+   		 playSound("honer-woohoo.wav");
+   	
    		 // 17. ...use the playSound method to play a drum sound. Test to see if it works
 
  
